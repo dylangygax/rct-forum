124ad729f0ee4036a812f19ff2695306e5c97bdb
DylanGygax <dylangygax@ncf.edu>
2020-09-12 18:08:11 -0500

    routes: folder created, index and users written

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const connectionString = "mongodb://localhost:27017/rct-forum"
+const configOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+
+mongoose.connect(connectionString, configOptions)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.log(`MongoDB connections error: ${err}`))
+
+module.exports = {
+    User: require('./user'),
+    Park: require('./park'),
+    Screenshot: require('./screenshot')
+}
