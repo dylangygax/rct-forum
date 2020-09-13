8805e8a0db9c7b708c54c886bbffe7f764af8bd0
DylanGygax <dylangygax@ncf.edu>
2020-09-13 15:39:24 -0500

    all user routes tested and working properly

--- controllers/users.js
+++ controllers/users.js
@@ -6,34 +6,35 @@ const db = require('../models')
 const index = (req, res) => {
     db.User.find({}, (err, foundUsers) => {
         if (err) console.log(`error in users#index: ${err}`)
-        res.send('users index called!')
+        //res.send('users index called!')
+        res.status(200).json({users: foundUsers})
     })
 }
 
 const show = (req,res) => {
     db.User.findById(req.params.id, (err, foundUser) => {
         if (err) console.log(`error in users#show: ${err}`)
-        res.send(`user show called`)
-        //res.status(200).json({user: foundUser})
+        //res.send(`user show called`)
+        res.status(200).json({user: foundUser})
     })
 }
 
 const create = (req, res) => {
     db.User.create(req.body, (err,createdUser) => {
         if (err) console.log(`error in users#create: ${err}`)
-        res.send(`user create called`)
-        //res.status(200).json({user: createdUser})
+        //res.send(`user create called`)
+        res.status(200).json({user: createdUser})
     })
 }
 
 const update = (req, res) => {
     db.User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
         if (err) console.log(`error in users#update: ${err}`)
-        res.send(`user update called`)
-        // res.status(200).json({
-        //     user: updatedUser,
-        //     //message: `${updatedUser.username} updated succesfully`
-        // })
+        //res.send(`user update called`)
+        res.status(200).json({
+            user: updatedUser,
+            //message: `${updatedUser.username} updated succesfully`
+        })
     })
 }
 
