d624e54677f8e96e6da1b3a56d7d7da55f48e4ce
DylanGygax <dylangygax@ncf.edu>
2020-09-14 13:17:14 -0500

    seed.js set up

--- seed.js
+++ seed.js
@@ -1,4 +1,17 @@
 const db = require('./models')
 const data = require('./seedData.json')
 
-//need to figure out how to do this asynchronously to avoid errors
+const seed = async () => {
+    await db.User.deleteMany({})
+    await db.Park.deleteMany({})
+    await db.Screenshot.deleteMany({})
+    await db.Comment.deleteMany({})
+    const createdUsers = db.User.create(data.users)
+    const createdParks = db.Park.create(data.users)
+    const createdScreenshots = db.Screenshot.create(data.users)
+    const createdComments = db.Comment.create(data.users)
+    console.log({users: createdUsers, parks: createdParks, screenshots: createdScreenshots, comments: createdComments})
+    process.exit()
+}
+
+seed()
